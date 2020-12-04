3b83750f2cff5dcb777e971ba1d67d633407d574
Kirukaii <gamerpros450@gmail.com>
2020-12-04 21:42:59 +0100

    command profile

--- commands/main/profile.js
+++ commands/main/profile.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, GuildMember } = require('discord.js');
 const chalk = require('chalk');
 module.exports = {
     name: 'profile',
@@ -6,17 +6,43 @@ module.exports = {
     description: 'shows profile',
     category: 'main',
 	execute(message, client, args, log) {
+
+        var user = message.mentions.users.first();
         var author = message.author;
         
-        var nickname = message.member.nickname;
+        
+        
         if (!nickname) {
           nickname = "brak"
         }
 
-        if (args[0]) {
-            
+        if (!user) {
+          var nickname = message.member.nickname;
+          const dataC = message.member.user.createdAt;
+          const dataJ = message.member.guild.joinedAt
 
-        }else {
+          Number.prototype.padLeft = function(base,chr){
+            var  len = (String(base || 10).length - String(this).length)+1;
+            return len > 0? new Array(len).join(chr || '0')+this : this;
+          }
+
+          
+          dCformat = [dataC.getDate().padLeft(),
+                    (dataC.getMonth()+1).padLeft(),
+                    dataC.getFullYear()].join('/')+' '+
+                    [dataC.getHours().padLeft(),
+                     dataC.getMinutes().padLeft(),
+                     dataC.getSeconds().padLeft()].join(':');
+
+          dJformat = [dataJ.getDate().padLeft(),
+                    (dataJ.getMonth()+1).padLeft(),
+                    dataJ.getFullYear()].join('/')+' '+
+                  [dataJ.getHours().padLeft(),
+                    dataJ.getMinutes().padLeft(),
+                    dataJ.getSeconds().padLeft()].join(':');         
+          
+          
+          
           const myProfileEmbed = new MessageEmbed()
           .setColor('#42f58a')
           .setTitle("Twój Profil")
@@ -25,12 +51,57 @@ module.exports = {
           .addField("ID:", author.id, true)
           .addField("Pseudonim:", nickname, true)
           .addField("Role:", `<@&${message.guild.member(author)._roles.join('> <@&')}>`)
-          .addField("Konto stworzone:")
-          .addField("Dołączenie do serwera:", message.author.joinedAt)
+          .addField("Konto stworzone:", dCformat)
+          .addField("Dołączenie do serwera:", dJformat )
           .setTimestamp()
           .setFooter('Yuna', client.user.displayAvatarURL());
           
          message.channel.send(myProfileEmbed);
+
+        }else {
+
+          var nickname = user.nickname;
+
+          const dataC = user.createdAt;
+          const dataJ = message.guild.member(user).joinedAt
+
+          Number.prototype.padLeft = function(base,chr){
+            var  len = (String(base || 10).length - String(this).length)+1;
+            return len > 0? new Array(len).join(chr || '0')+this : this;
+          }
+
+          
+          dCformat = [dataC.getDate().padLeft(),
+                    (dataC.getMonth()+1).padLeft(),
+                    dataC.getFullYear()].join('/')+' '+
+                    [dataC.getHours().padLeft(),
+                     dataC.getMinutes().padLeft(),
+                     dataC.getSeconds().padLeft()].join(':');
+
+          dJformat = [dataJ.getDate().padLeft(),
+                    (dataJ.getMonth()+1).padLeft(),
+                    dataJ.getFullYear()].join('/')+' '+
+                  [dataJ.getHours().padLeft(),
+                    dataJ.getMinutes().padLeft(),
+                    dataJ.getSeconds().padLeft()].join(':');         
+          
+          
+          
+          const myProfileEmbed = new MessageEmbed()
+          .setColor('#42f58a')
+          .setTitle("Twój Profil")
+          .setThumbnail(author.displayAvatarURL())
+          .addField("Nazwa:", user.username, true)
+          .addField("ID:", user.id, true)
+          .addField("Pseudonim:", nickname, true)
+          .addField("Role:", `<@&${message.guild.member(user)._roles.join('> <@&')}>`)
+          .addField("Konto stworzone:", dCformat)
+          .addField("Dołączenie do serwera:", dJformat )
+          .setTimestamp()
+          .setFooter('Yuna', client.user.displayAvatarURL());
+          
+         message.channel.send(myProfileEmbed);
+         
         }
 
 	},
